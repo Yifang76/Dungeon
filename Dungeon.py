9d570fb17c124524229955bbb53b494a0a39ba87
Yifang He <105815046+Yifang76@users.noreply.github.com>
2023-02-18 10:27:13 +0000

    1

--- Dungeon.py
+++ Dungeon.py
@@ -2,31 +2,19 @@ import random
 
 name = input("Name, please ")
 
-def noun(one, two, three):
-    nlist = one, two, three
-    n = random.choice(nlist)
-    if n == "warlord" or n == "dictator":
-        det = str("a")
-    else:
-        det = str("an")
-    return n
+nlist = "imp","warlord","dictator"
+n = random.choice(nlist)
+if n == "warlord" or n == "dictator":
+    det = str("a")
+else:
+    det = str("an")
+
+vlist = "stands","looks","glares"
+v = random.choice(vlist)
 
 
-noun("imp","warlord","dictator")
-
-def verb(one, two, three):
-    vlist = one, two, three
-    v = random.choice(vlist)
-    return v
-
-verb("stands","looks","glares")
-
-def playact(one, two, three):
-    palist = one, two, three
-    pv = random.choice(palist)
-    return pv
-
-playact("saunter","walk","run")
+palist = "saunter","walk","run"
+pv = random.choice(palist)
 
 print("As you "+pv+" down the path, "+det+ " "+n+ " "+v+ " there menacingly. What will you do? ")
 
