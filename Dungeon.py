816d54d2a4904091f56f69a9b393957adb255fb3
Yifang He <105815046+Yifang76@users.noreply.github.com>
2023-02-18 11:10:45 +0000

    3

--- Dungeon.py
+++ Dungeon.py
@@ -1,13 +1,24 @@
 import random
 
 name = input("Name, please ")
+print("Knight: Honourable warriors of the Lennish empire, they are extremely strong and are representatives of the law. (Strength: 10, Agility: 3, Dexterity: 8, Health: 7, Perception: 4, Charisma: 6 (Within lawful lands) or 0 (in foreign lands), Intelligence: 5 )")
+print("Merchant: (Strength: 1, Agility: 8, Dexterity: 1, Health: 3, Perception: 8, Charisma: 10, Intelligence: 10 )")
+
+classn = input("Pick a class ").capitalize()
+
+def classo(chosen, cl):
+    if classn == input(chosen):
+        print("Welcome to the game, "+cl+" "+name+".")
+
+classo("Knight", "knight")
+classo("Merchant", "merchant")
+
+
+
+
 
 nlist = "imp","warlord","dictator"
 n = random.choice(nlist)
-if n == "warlord" or n == "dictator":
-    det = str("a")
-else:
-    det = str("an")
 
 vlist = "stands","looks","glares"
 v = random.choice(vlist)
@@ -16,12 +27,21 @@ v = random.choice(vlist)
 palist = "saunter","walk","run"
 pv = random.choice(palist)
 
-t = str(" "+det+" "+n+" "+v+" ")
+if n == "warlord" or n == "dictator":
+    det = str("a")
+else:
+    det = str("an")
+
+if v == "looks" or v == "glares":
+    ex = str("at you")
+else:
+    ex = str("there ")
+t = str(" "+det+" "+n+" "+v+" "+ex+" ")
 
 
 
-print("As you "+pv+" down the path,"+t+"there menacingly. What will you do? ")
 
+opo = int(input("As you "+pv+" down the path,"+t+"menacingly. What will you do? "))
 
 
 
